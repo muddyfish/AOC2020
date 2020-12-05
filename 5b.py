9f82a28f160a23824719999827609dcd8eb4a2f4
Blue <blue@nqn.blue>
2020-12-05 09:59:35 +0000

    Slightly more concise

--- 5b.py
+++ 5b.py
@@ -1,9 +1,7 @@
 translation = str.maketrans("FBLR", "0101", "\n")
-seat_ids = set()
 
 with open("5.txt") as f:
-    for i in f.readlines():
-        seat_ids.add(int(i.translate(translation), 2))
+    seat_ids = {int(i.translate(translation), 2) for i in f.readlines()}
 
 min_id = min(seat_ids)
 max_id = max(seat_ids)
